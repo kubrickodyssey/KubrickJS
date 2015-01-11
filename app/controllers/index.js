d5c3fd2acbe664b817e498ae4185d0d3834497ef
Jorge Luis Juarez <jjuarez007@gmail.com>
2015-01-11 13:37:32 -0600

    index y api controller

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -6,25 +6,20 @@ module.exports.index = {
 		var mongoose = require('mongoose');
 		var db = mongoose.createConnection( 'mongodb://localhost:27017/apiv1' );
 
-
-		var apiTypeSchema = mongoose.Schema({
-			typeName: { type : String, trim : true, index : true, unique: true, required: true}
-		});
-
 		var apiSchema = mongoose.Schema({
 		    url: { type : String, trim : true, index : true, required: true, unique: true },
 		    title: { type : String, trim : true },
-		    type: {type: mongoose.Schema.Types.ObjectId, ref: 'apiTypeSchema'},
-		    api: {type: mongoose.Schema.Types.ObjectId, ref: 'apiSchema'}
+		    type: String,
+		    api: String
 		});
 
 		var Api = db.model('apis', apiSchema);
-		var ApiType = db.model('types', apiTypeSchema)
 
 		var this_controller = this;
 
 
-		Api.find(function(err, data){
+		Api.find({'type':'index'},function(err, data){
+			console.error(err);
 			this_controller.set('title', 'Kubrick Backstage');
 			this_controller.set('site_title', 'Kb');
 			this_controller.set('apis', data);
